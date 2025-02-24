3b550f24be2c4ce22465b3f47216650579d86007
Mason <47919925+OAOEnder@users.noreply.github.com>
2025-02-24 13:40:50 -0600

    rerouted img tags, changed App to tsx

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './code/screens/HomeScreen';
+import SignInScreen from './code/screens/SignInScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator 
+        initialRouteName="SignIn" 
+        screenOptions={{ headerTitle: 'Axis' }}>
+          
+        <Stack.Screen name="SignIn" component={SignInScreen} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
