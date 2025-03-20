ac35e7ae3ba8d5f25964a0ec99bbb99276d764ae
xerioc2 <xerioc2@hotmail.com>
2025-03-19 22:25:14 -0500

    started teacherdashboard and mean comment to tyler about BG

--- code/screens/TeacherDashboard.tsx
+++ code/screens/TeacherDashboard.tsx
@@ -1,16 +1,37 @@
-import { Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, ImageBackground, TouchableOpacity, StyleSheet } from 'react-native';
 
 
 
 const TeacherDashboard: React.FC = () => {
 
 
-    return (
-        <>
+    return<>      
+     <View style={styles.container}>
+                    <ImageBackground source={require('../assets/images/axis-bg.png')} style={styles.backgroundImage} resizeMode="cover">
             <Text>TEACHER DASHBOARD</Text>
+
+            </ImageBackground>
+            </View>
         </>
-    )
+    
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#E8F5E9',
+        padding: 2,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 0,
+        paddingVertical: 0
+    },
+    backgroundImage: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+    }
+});
 
 
 export default TeacherDashboard;
