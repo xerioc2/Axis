d03a0a05ee5f6c09c7581e20c0b2a6f426e7c975
mason <masongkrause@yahoo.com>
2025-04-09 19:00:15 -0500

    merge at a stable point, need functionality onPress's

--- code/components/teacherDashboard/TeacherDashboardMenu.tsx
+++ code/components/teacherDashboard/TeacherDashboardMenu.tsx
@@ -1,75 +1,66 @@
-import React, { SetStateAction, useRef } from 'react';
+import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Animated,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import { styles } from "./TeacherDashboardStyle";
-import { View, Text, TouchableOpacity, Modal, Animated,
-  KeyboardAvoidingView, Platform, ScrollView, TouchableWithoutFeedback, Keyboard} from "react-native";
-
 
 type TeacherDashboardMenuProps = {
   closeModal: () => void;
-}
+  slideAnim: Animated.Value;
+};
 
-const TeacherDashboardMenu: React.FC<TeacherDashboardMenuProps> = ({closeModal}) => {
-    
-  const slideAnim = useRef(new Animated.Value(500)).current;
+const TeacherDashboardMenu: React.FC<TeacherDashboardMenuProps> = ({
+  closeModal,
+  slideAnim,
+}) => {
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.modalOverlay}>
+        <Animated.View
+          style={[styles.modalContainer, { transform: [{ translateY: slideAnim }] }]}
+        >
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={{ flex: 1 }}
+          >
+            <ScrollView
+              contentContainerStyle={{ paddingBottom: 100 }}
+              keyboardShouldPersistTaps="handled"
+            >
+              <TouchableOpacity style={styles.modalButton}>
+                <Text style={styles.modalButtonText}>Add Section</Text>
+              </TouchableOpacity>
 
-    return <>
-        <View>
-            <Modal transparent visible={true} animationType="none">
-              <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                <View style={styles.modalOverlay}>
-                  <Animated.View
-                    style={[
-                      styles.modalContainer,
-                      { transform: [{ translateY: slideAnim }] },
-                    ]}
-                  >
-                    <KeyboardAvoidingView
-                      behavior={Platform.OS === "ios" ? "padding" : "height"}
-                      style={{ flex: 1 }}
-                    >
-                      <ScrollView
-                        contentContainerStyle={{ paddingBottom: 100 }}
-                        keyboardShouldPersistTaps="handled"
-                      >
-                        
-                      <>
-                          <TouchableOpacity
-                              style={styles.modalButton}
-                              
-                          >
-                              <Text style={styles.modalButtonText}>Add Section</Text>
-                          </TouchableOpacity>
-      
-                          <TouchableOpacity
-                              style={styles.modalButton}
-                          
-                          >
-                              <Text style={styles.modalButtonText}>Add Course</Text>
-                          </TouchableOpacity>
-      
-                          <TouchableOpacity 
-                              style={styles.modalButton}
-      
-                          >
-                              <Text style={styles.modalButtonText}> Delete Course</Text>
-                          </TouchableOpacity>
-      
-                          <TouchableOpacity
-                          style={[styles.modalButton, { backgroundColor: "#ccc" }]}
-                          onPress={() => closeModal()}
-                          >
-                          <Text style={styles.modalButtonText}>Cancel</Text>
-                          </TouchableOpacity>
-                      </>
-                        <View style={{ height: 200 }} />
-                      </ScrollView>
-                    </KeyboardAvoidingView>
-                  </Animated.View>
-                </View>
-              </TouchableWithoutFeedback>
-            </Modal>
-          </View>
-      </>
+              <TouchableOpacity style={styles.modalButton}>
+                <Text style={styles.modalButtonText}>Add Course</Text>
+              </TouchableOpacity>
 
-}
+              <TouchableOpacity style={styles.modalButton}>
+                <Text style={styles.modalButtonText}>Delete Course</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={[styles.modalButton, { backgroundColor: "#ccc" }]}
+                onPress={closeModal}
+              >
+                <Text style={styles.modalButtonText}>Cancel</Text>
+              </TouchableOpacity>
+
+              <View style={{ height: 200 }} />
+            </ScrollView>
+          </KeyboardAvoidingView>
+        </Animated.View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
 export default TeacherDashboardMenu;
--- code/screens/TeacherDashboard.tsx
+++ code/screens/TeacherDashboard.tsx
@@ -1,77 +1,154 @@
-import React, { useState, useRef } from "react";
-import { View, Text, FlatList, Image, TouchableOpacity, Modal, Animated,
-    KeyboardAvoidingView, Platform, ScrollView, TouchableWithoutFeedback, Keyboard} from "react-native";
+import React, { useState, useRef, useEffect } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  Modal,
+  Animated,
+  Platform,
+  KeyboardAvoidingView,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { styles } from "../components/teacherDashboard/TeacherDashboardStyle";
 import { getTeacherData } from "../service/supabaseService";
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import type { User, Course, TeacherDataDto, SectionPreviewDto } from '../../App';
-import { useRoute, useNavigation, RouteProp } from '@react-navigation/native';
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import type {
+  User,
+  Course,
+  TeacherDataDto,
+  SectionPreviewDto,
+} from "../../App";
+import {
+  useRoute,
+  useNavigation,
+  RouteProp,
+} from "@react-navigation/native";
 import type { RootStackParamList } from "../utils/navigation.types";
 import ErrorMessage from "../components/ErrorMessage";
 import TeacherDashboardMenu from "../components/teacherDashboard/TeacherDashboardMenu";
+import { useFonts } from "expo-font";
 
-
-type TeacherDashboardRouteProp = RouteProp<RootStackParamList, 'TeacherDashboard'>;
-type NavigationProps = NativeStackNavigationProp<RootStackParamList, 'TeacherDashboard'>;
+type TeacherDashboardRouteProp = RouteProp<
+  RootStackParamList,
+  "TeacherDashboard"
+>;
+type NavigationProps = NativeStackNavigationProp<
+  RootStackParamList,
+  "TeacherDashboard"
+>;
 
 const TeacherDashboard: React.FC = () => {
-    const navigation = useNavigation<NavigationProps>();
-    const route = useRoute<TeacherDashboardRouteProp>();
-    const teacher = route.params;
+  const navigation = useNavigation<NavigationProps>();
+  const route = useRoute<TeacherDashboardRouteProp>();
+  const teacher = route.params;
 
-    const slideAnim = useRef(new Animated.Value(500)).current;
-  
-    
-    const [modalVisible, setModalVisible] = useState(false);
+  const slideAnim = useRef(new Animated.Value(500)).current;
 
+  const [sectionPreviews, setSectionPreviews] = useState<SectionPreviewDto[]>(
+    []
+  );
+  const [coursesCreated, setCoursesCreated] = useState<Course[]>([]);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [selectedMenuOption, setSelectedMenuOption] = useState<string>(
+    "sections"
+  );
+  const [modalVisible, setModalVisible] = useState(false);
 
-    const openModal = () => {
-        setModalVisible(true);
-        Animated.timing(slideAnim, {
-            toValue: 0,
-            duration: 300,
-            useNativeDriver: true,
-        }).start();
-    }
-    const closeModal = () => {
-        Animated.timing(slideAnim, {
-            toValue: 500,
-            duration: 300,
-            useNativeDriver: true,
-        }).start(() => setModalVisible(false));
-    }
+  const [fontsLoaded] = useFonts({
+    "SF Pro": require("../assets/fonts/sf_pro.ttf"),
+  });
 
+  useEffect(() => {
+    const fetchTeacherData = async (teacher: User) => {
+      let teacherData: TeacherDataDto = await getTeacherData(teacher.user_id);
+      if (!teacherData) {
+        setErrorMessage(
+          "Uh oh... looks like you may not have taught any sections yet. When teaching a section, it will be displayed here."
+        );
+        return;
+      }
+      setSectionPreviews(teacherData.sections);
+      setCoursesCreated(teacherData.courses_created);
+      console.log("Teacher data loaded successfully");
+    };
+    setErrorMessage("");
+    fetchTeacherData(teacher);
+  }, []);
 
+  const openModal = () => {
+    setModalVisible(true);
+    Animated.timing(slideAnim, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const closeModal = () => {
+    Animated.timing(slideAnim, {
+      toValue: 500,
+      duration: 300,
+      useNativeDriver: true,
+    }).start(() => setModalVisible(false));
+  };
 
   return (
-    <View  style={styles.container}>
-      <Text style={styles.title}>Welcome to your dashboard, {teacher.first_name}</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Welcome to your dashboard, {teacher.first_name}
+      </Text>
 
-      {/* This will have sectionPreviewDtos not courses */}
-      <FlatList
-        data={courses} 
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <View style={styles.card}>
-            <Image source={{ uri: item.image }} style={styles.image} />
-            <Text style={styles.courseTitle}>{item.name}</Text>
-            <Text style={styles.courseCode}>{item.code}</Text>
-          </View>
-        )}
-      />
+      {selectedMenuOption === "sections" && (
+        <FlatList
+          data={sectionPreviews}
+          keyExtractor={(section) => section.section_id.toString()}
+          renderItem={({ item }) => (
+            <View style={styles.card}>
+              <Text style={styles.courseTitle}>{item.course_name}</Text>
+              <Text style={styles.courseCode}>
+                {item.course_subject} {item.course_identifier}{" "}
+                {item.section_identifier}
+              </Text>
+            </View>
+          )}
+        />
+      )}
 
+      {selectedMenuOption === "courses" && (
+        <FlatList
+          data={coursesCreated}
+          keyExtractor={(course) => course.course_id.toString()}
+          renderItem={({ item }) => (
+            <View style={styles.card}>
+              <Text style={styles.courseTitle}>{item.course_name}</Text>
+              <Text style={styles.courseCode}>
+                {item.course_subject} {item.course_identifier}
+              </Text>
+              <Text>Date Created: {item.date_created}</Text>
+            </View>
+          )}
+        />
+      )}
 
-      {/*Footer*/}
+      {/* Footer */}
       <View style={styles.footer}>
         <TouchableOpacity style={styles.editButton} onPress={openModal}>
           <Ionicons name="ellipsis-vertical" size={32} color="white" />
         </TouchableOpacity>
+        
       </View>
 
-        {modalVisible && <TeacherDashboardMenu closeModal={closeModal} />}
+      {modalVisible && (
+        <Modal transparent animationType="none" visible={modalVisible}>
+          <TeacherDashboardMenu closeModal={closeModal} slideAnim={slideAnim} />
+        </Modal>
+      )}
 
-      
+      {errorMessage !== "" && <ErrorMessage message={errorMessage} />}
     </View>
   );
 };
