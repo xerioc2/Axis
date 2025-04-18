775f849ae2f38fcb0213e7e778e3aa7f378d9ccc
xerioc2 <xerioc2@hotmail.com>
2025-04-18 15:56:50 -0500

    fixed wrong naming in StudentDashboard.tsx

--- code/screens/student/StudentDashboard.tsx
+++ code/screens/student/StudentDashboard.tsx
@@ -5,7 +5,7 @@ import {
     TouchableOpacity,
     StyleSheet,
 } from "react-native";
-import { styles } from "../../components/studentDashboard/StudentDashboardStyle"; 
+import { styles } from "../../components/StudentDashboard/StudentDashboardStyle"; 
 import { getStudentData, enrollInSection } from "../../service/supabaseService"; 
 import type {
     User,
@@ -18,7 +18,7 @@ import {
 } from "@react-navigation/native";
 import type { RootStackParamList } from "../../utils/navigation.types";
 import ErrorMessage from "../../components/ErrorMessage"; 
-import SectionCardList from "../../components/studentDashboard/SectionCardList";
+import SectionCardList from "../../components/StudentDashboard/SectionCardList";
 import { TextInput } from "react-native";
 
 
