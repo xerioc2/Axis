fe5b8fdeb48732adc2ade7de3ab0f3e87e090141
Tyler Gilbert <tylergilber517@gmail.com>
2025-04-23 11:12:19 -0500

    Raised height of footer in teacher dash

--- code/components/footer/StudentFooter.tsx
+++ code/components/footer/StudentFooter.tsx
@@ -1,40 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, ImageBackground, TouchableOpacity, StyleSheet } from 'react-native';
-
-
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  ImageBackground,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 
 export default function Header() {
   const [showCourses, setShowCourses] = useState(false);
   const [showCheckPoints, setShowCheckPoints] = useState(false);
   const [showAddClass, setShowAddClass] = useState(false);
   const [showSemester, setShowSemester] = useState(false);
-
-  };
-
+}
 
 const StudentHeader: React.FC = () => {
-    return<>      
-     <View style={styles.container}>
-                    <ImageBackground source={require('../assets/images/axis-bg.png')} style={styles.backgroundImage} resizeMode="cover">
-            <Text>Student Header</Text>
-
-            </ImageBackground>
-            </View>
-        </>
-}
+  return (
+    <>
+      <View style={styles.container}>
+        <ImageBackground
+          source={require("../assets/images/axis-bg.png")}
+          style={styles.backgroundImage}
+          resizeMode="cover"
+        >
+          <Text>Student Header</Text>
+        </ImageBackground>
+      </View>
+    </>
+  );
+};
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#E8F5E9',
-        padding: 2,
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 0,
-        paddingVertical: 0
-    },
-    backgroundImage: {
-        flex: 1,
-        width: '100%',
-        height: '100%',
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#E8F5E9",
+    padding: 2,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 0,
+    paddingVertical: 0,
+  },
+  backgroundImage: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
 });
